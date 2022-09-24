15c095ee5b0bd64c4dfecc54331717e69f13d815
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-24 15:38:21 +0300

    formatting

--- Program.cs
+++ Program.cs
@@ -14,5 +14,5 @@ int yb = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите значение Z для B: ");
 int zb = int.Parse(Console.ReadLine()!);
 
-double l = Math.Sqrt(Math.Pow(xa-xb, 2) + Math.Pow(ya-yb, 2) + Math.Pow(za-zb, 2));
+double l = Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2) + Math.Pow(za - zb, 2));
 Console.WriteLine($"Расстояние между точками = {l:f2}");
